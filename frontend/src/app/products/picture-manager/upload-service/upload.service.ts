d45fcb08644035b37fcdca8e1bbd9599092d7758
Simone Egli <simone.egli@protonmail.ch>
2020-10-16 15:52:12 +0200

    Implement first try of picture manager according the tutorial on https://www.ahmedbouchefra.com/angular-tutorial-example-upload-files-with-formdata-httpclient-rxjs-and-material-progressbar/

---
+++ frontend/src/app/products/picture-manager/upload-service/upload.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from  '@angular/common/http';  
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+  SERVER_URL: string = "http://localhost:4200"; // To be corrected, here comes the file upload server
+  
+  constructor(private httpClient: HttpClient) { }
+  
+  public upload(formData) {
+
+    return this.httpClient.post<any>(this.SERVER_URL, formData, {  
+        reportProgress: true,  
+        observe: 'events'  
+      });  
+  }
+}
